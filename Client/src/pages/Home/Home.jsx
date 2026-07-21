19eb3b2ef7cbdc1c3c674244b4dab2a3c0c61987
mounika22-ui <mounikasista95@gmail.com>
2026-07-21 10:16:12 +0530

    updated home

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,10 +1,20 @@
 import "./Home.css";
 import Hero from "../../components/Hero/Hero";
 import Categories from "../../components/Categories/Categories";
+import bg from "../../assets/images/bg.jpg";
 
 function Home() {
   return (
-    <div className="home">
+    <div
+      className="home"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}
+    >
       <Hero />
       <Categories />
     </div>
