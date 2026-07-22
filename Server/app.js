045dedf2f7f592f41b20901de4be493769634a97
mounika22-ui <mounikasista95@gmail.com>
2026-07-22 06:55:35 +0530

    update my app

--- Server/app.js
+++ Server/app.js
@@ -3,33 +3,51 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./config/db.js";
+
+import logger from "./middleware/logger.js";
+
 import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-// Connect to MongoDB
+// Connect MongoDB
 connectDB();
 
-// Middleware
+// Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 // Home Route
 app.get("/", (req, res) => {
-  res.json({
+  res.status(200).json({
     success: true,
-    message: "Welcome to ColorSpun Charms ",
+    message: "Welcome to ColorSpun Charms Backend ",
   });
 });
 
-// Authentication Routes
+// Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
 
-// Server
+// Invalid Route
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+  console.log(` Server Running on http://localhost:${PORT}`);
 });
