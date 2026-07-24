6aa6092ae241f8cf5a58f48613569ad34ef64120
mounika22-ui <mounikasista95@gmail.com>
2026-07-24 12:08:21 +0530

    productroutes

--- Server/routes/productRoutes.js
+++ Server/routes/productRoutes.js
@@ -10,14 +10,11 @@ import {
 
 const router = express.Router();
 
+// Product Routes
 router.get("/", getProducts);
-
 router.get("/:id", getProductById);
-
 router.post("/", addProduct);
-
 router.put("/:id", updateProduct);
-
 router.delete("/:id", deleteProduct);
 
 export default router;
