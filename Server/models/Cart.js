ffd0e8eca68a755eb965506ad4849e1f517cad44
mounika22-ui <mounikasista95@gmail.com>
2026-07-24 12:06:36 +0530

    cart

--- Server/models/Cart.js
+++ Server/models/Cart.js
@@ -2,12 +2,6 @@ import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-
     productId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Product",
@@ -17,6 +11,7 @@ const cartSchema = new mongoose.Schema(
     quantity: {
       type: Number,
       default: 1,
+      min: 1,
     },
   },
   {
@@ -24,4 +19,6 @@ const cartSchema = new mongoose.Schema(
   }
 );
 
-export default mongoose.model("Cart", cartSchema);
+const Cart = mongoose.model("Cart", cartSchema);
+
+export default Cart;
